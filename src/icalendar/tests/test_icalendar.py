378be86a0243293e6727fde020a92d63d9796954
Christian Geier <geier@lostpackets.de>
2016-12-30 13:57:37 +0100

    Moved test_apple_xlocation() to test_icalendar.py

--- src/icalendar/tests/test_icalendar.py
+++ src/icalendar/tests/test_icalendar.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import textwrap
+import icalendar
+import os
 
 from icalendar.tests import unittest
 
@@ -281,3 +282,20 @@ class IcalendarTestCase (unittest.TestCase):
         from ..parser import q_join
         self.assertEqual(q_join(['Max', 'Moller', 'Rasmussen, Max']),
                          'Max,Moller,"Rasmussen, Max"')
+
+
+class TestEncoding(unittest.TestCase):
+
+    def test_apple_xlocation(self):
+        """
+        Test if error messages are encode properly.
+        """
+        try:
+            directory = os.path.dirname(__file__)
+            data = open(os.path.join(directory, 'x_location.ics'), 'rb').read()
+            cal = icalendar.Calendar.from_ical(data)
+            for event in cal.walk('vevent'):
+                self.assertEqual(len(event.errors), 0, 'Got too many errors')
+
+        except UnicodeEncodeError as e:
+            self.fail("There is something wrong with encoding in the collected error messages")
